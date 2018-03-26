eba9de092eb771f102394cf917005365a6d038a6
Kevin Carrogan <kevin.carrogan@gmail.com>
2018-03-27 00:45:34 +0100

    Dynamically displaying drivers with basic information.

--- src/components/TimingTower.js
+++ src/components/TimingTower.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 import '../css/timing-tower.css';
 
+const driverNames = [
+    'Hamilton',
+    'Bottas',
+    'Raikkonen',
+    'Verstappen',
+    'Ricciardo',
+    'Vettel'
+];
+
+const getDriverShortName = name => name.substring(0, 3);
+
 const TimingTower = () => {
     return (
         <section className="timing-tower">
@@ -10,36 +21,13 @@ const TimingTower = () => {
                 <div>1:17:59</div>
             </div>
             <ol className="driver-times">
-                <li className="driver-time driver-time--on-track">
-                    <span className="driver-position">1</span>
-                    <span className="driver-name mercedes">HAM</span>
-                    <span className="lap-status lap-status--first">1:24.569</span>
-                </li>
-                <li className="driver-time driver-time--on-track">
-                    <span className="driver-position">2</span>
-                    <span className="driver-name mercedes">BOT</span>
-                    <span className="lap-status lap-status--interval">+0.150</span>
-                </li>
-                <li className="driver-time driver-time--on-track driver-time--personal-best">
-                    <span className="driver-position">3</span>
-                    <span className="driver-name ferrari">RAI</span>
-                    <span className="lap-status lap-status--interval">1:25.828</span>
-                </li>
-                <li className="driver-time driver-time--in-pit">
-                    <span className="driver-position">4</span>
-                    <span className="driver-name red-bull">VER</span>
-                    <span className="lap-status lap-status--interval">+0.402</span>
-                </li>
-                <li className="driver-time driver-time--on-track">
-                    <span className="driver-position">5</span>
-                    <span className="driver-name red-bull">RIC</span>
-                    <span className="lap-status lap-status--interval">+0.457</span>
-                </li>
-                <li className="driver-time driver-time--out-lap">
-                    <span className="driver-position">6</span>
-                    <span className="driver-name ferrari">VET</span>
-                    <span className="lap-status lap-status--interval">OUT LAP</span>
-                </li>
+                {driverNames.map((driverName, position) => (
+                    <li className="driver-time" key={driverName}>
+                        <span className="driver-position">{position + 1}</span>
+                        <span className="driver-name">{getDriverShortName(driverName)}</span>
+                        <span className="lap-status"></span>
+                    </li>
+                ))}
             </ol>
         </section>
     );
