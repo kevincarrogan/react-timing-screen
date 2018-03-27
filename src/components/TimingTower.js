dd4454dfabcb3f45eb9935b4dc6c2b73278d250f
Kevin Carrogan <kevin.carrogan@gmail.com>
2018-03-27 13:04:43 +0100

    Adding driver times.

--- src/components/TimingTower.js
+++ src/components/TimingTower.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import '../css/timing-tower.css';
 
-const driverNames = [
-    'Hamilton',
-    'Bottas',
-    'Raikkonen',
-    'Verstappen',
-    'Ricciardo',
-    'Vettel'
+const driverStatuses = [
+    ['Hamilton', '1:24.569'],
+    ['Bottas', '1:24.719'],
+    ['Raikkonen', '1:25.828'],
+    ['Verstappen', '1:25.929'],
+    ['Ricciardo', '1:26.201'],
+    ['Vettel', '1:26.450']
 ];
 
 const getDriverShortName = name => name.substring(0, 3);
@@ -21,11 +21,11 @@ const TimingTower = () => {
                 <div>1:17:59</div>
             </div>
             <ol className="driver-times">
-                {driverNames.map((driverName, position) => (
-                    <li className="driver-time" key={driverName}>
+                {driverStatuses.map((driverStatus, position) => (
+                    <li className="driver-time" key={driverStatus[0]}>
                         <span className="driver-position">{position + 1}</span>
-                        <span className="driver-name">{getDriverShortName(driverName)}</span>
-                        <span className="lap-status"></span>
+                        <span className="driver-name">{getDriverShortName(driverStatus[0])}</span>
+                        <span className="lap-status">{driverStatus[1]}</span>
                     </li>
                 ))}
             </ol>
