c86d96d62a65b13703a6354314ddd1c0f1cacc92
Kevin Carrogan <kevin.carrogan@gmail.com>
2018-03-27 19:03:58 +0100

    Displaying time delta.

--- src/components/TimingTower.js
+++ src/components/TimingTower.js
@@ -4,16 +4,60 @@ import '../css/timing-tower.css';
 
 const driverStatuses = [
     ['Hamilton', '1:24.569'],
-    ['Bottas', '1:24.719'],
+    ['Bottas', '1:24.570'],
     ['Raikkonen', '1:25.828'],
-    ['Verstappen', '1:25.929'],
-    ['Ricciardo', '1:26.201'],
-    ['Vettel', '1:26.450']
+    ['Verstappen', '1:45.929'],
+    ['Ricciardo', '2:26.201'],
+    ['Vettel', '2:45.450']
 ];
 
 const getDriverShortName = name => name.substring(0, 3);
 
+const timeInMilliseconds = time => {
+    const components = time.split(/[:\.]/);
+    const minutes = parseInt(components[0], 10);
+    const seconds = parseInt(components[1], 10);
+    const milliseconds = parseInt(components[2], 10);
+
+    return (minutes * 60 * 1000) + (seconds * 1000) + milliseconds;
+};
+
+const getFastestTime = driverStatuses => driverStatuses
+    .map(
+        status => timeInMilliseconds(status[1])
+    )
+    .concat()
+    .sort((a, b) => a > b)[0];
+
+const getDelta = (currentTime, fastestTimeInMilliseconds) => {
+    const currentTimeInMilliseconds = timeInMilliseconds(currentTime);
+    const diff = currentTimeInMilliseconds - fastestTimeInMilliseconds;
+
+    return diff;
+};
+
+const humanReadableTimeFromMilliseconds = time => {
+    let totalMilliseconds = time;
+
+    const minutes = Math.floor(totalMilliseconds / (60 * 1000));
+    totalMilliseconds = totalMilliseconds - (minutes * 60 * 1000);
+
+    const seconds = Math.floor(totalMilliseconds / 1000);
+    totalMilliseconds = totalMilliseconds - (seconds * 1000);
+
+    const milliseconds = totalMilliseconds;
+    const paddedMilliseconds = milliseconds.toString().padStart(3, '0');
+
+    if (minutes > 0) {
+        const paddedSeconds = seconds.toString().padStart(2, '0');
+        return `${minutes}:${paddedSeconds}.${paddedMilliseconds}`;
+    }
+
+    return `${seconds}.${paddedMilliseconds}`;
+};
+
 const TimingTower = () => {
+    const fastestTime = getFastestTime(driverStatuses);
     return (
         <section className="timing-tower">
             <div className="session-details">
@@ -25,7 +69,7 @@ const TimingTower = () => {
                     <li className="driver-time" key={driverStatus[0]}>
                         <span className="driver-position">{position + 1}</span>
                         <span className="driver-name">{getDriverShortName(driverStatus[0])}</span>
-                        <span className="lap-status">{driverStatus[1]}</span>
+                        <span className="lap-status">+{humanReadableTimeFromMilliseconds(getDelta(driverStatus[1], fastestTime))}</span>
                     </li>
                 ))}
             </ol>
