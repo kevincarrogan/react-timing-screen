51f3048b8c9ae1b61270ca3e0c1792c94fa29411
Kevin Carrogan <kevin.carrogan@gmail.com>
2018-04-18 00:33:32 +0100

    Freezing and using Symbol to mimic an enum for constants.

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-const STATUS = {
-  ON_TRACK: 'ON_TRACK',
-  IN_PIT: 'IN_PIT',
-  OUT_LAP: 'OUT_LAP',
-};
+const STATUS = Object.freeze({
+  ON_TRACK: Symbol('ON_TRACK'),
+  IN_PIT: Symbol('IN_PIT'),
+  OUT_LAP: Symbol('OUT_LAP'),
+});
 
 export default STATUS;
